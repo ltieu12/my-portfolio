f6792a795cf339e54bb7d62b39900d6bcc3858db
Lam Tieu <tieunhatlam1210@gmail.com>
2025-01-06 22:51:23 -0400

    Add responsive style for Mobile Design section

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -5,19 +5,20 @@ import ProjectKeywords from "../components/ProjectKeywords";
 export const ProjectsPage = () => {
     return (
         <>
-            <div className="container mx-auto px-10 md:px-24 mt-20">
-                <h1 className="text-6xl md:text-7xl font-Clash text-left">What I've Done</h1>
+            {/* Web Dev Section */}
+            <div className="container mx-auto px-10 lg:px-24 mt-20">
+                <h1 className="text-6xl lg:text-7xl font-Clash text-left">What I've Done</h1>
                 <h2 className="text-4xl font-Clash font-semibold text-left mt-14 mb-6">Web Development</h2>
             </div>
 
-            <div className='container mx-auto px-10 md:px-24 mb-20 md:mb-32 md:flex md:flex-row justify-between items-center gap-10'>
-                <div className="md:max-w-1/2 md:text-left space-y-5">
-                    <div className="bg-custom-baby-blue px-6 md:px-10 pt-6 md:pt-8 pb-10 md:pb-14 shadow-project-shadow-left rounded-lg border-3 border-black">
+            <div className='container mx-auto px-10 lg:px-24 mb-20 lg:mb-32 lg:flex lg:flex-row justify-between items-center gap-10'>
+                <div className="lg:max-w-1/2 lg:text-left space-y-5">
+                    <div className="bg-custom-baby-blue px-6 lg:px-10 pt-6 lg:pt-8 pb-10 lg:pb-14 shadow-project-shadow-left rounded-lg border-3 border-black">
                         <img src="/projects-img/StudyPal.png" alt="StudyPal website"></img>
                     </div>
                 </div>
 
-                <div className="md:w-1/2 md:text-left mt-10 md:mt-0 text-left space-y-4">
+                <div className="lg:w-1/2 lg:text-left mt-10 lg:mt-0 text-left space-y-4">
                     <h3 className="text-3xl font-Clash font-semibold">StudyPal E-Learning Website</h3>
                     <ProjectKeywords bgColor="bg-yellow-200" keywords={["Vue.js", ".NET", "MongoDB"]} />
                     <p>StudyPal is an e-learning web app that connects students with tutors or study groups for collaboration in their courses. I worked in a team of 4, focusing on designing and developing the user profile feature.</p>
@@ -27,8 +28,8 @@ export const ProjectsPage = () => {
                 </div>
             </div>
 
-            <div className='container mx-auto px-10 md:px-24 mb-20 md:mb-32 flex flex-col-reverse md:flex-row justify-between items-center gap-10'>
-                <div className="md:w-1/2 md:text-left text-left space-y-4">
+            <div className='container mx-auto px-10 lg:px-24 mb-20 lg:mb-32 flex flex-col-reverse lg:flex-row justify-between items-center gap-10'>
+                <div className="lg:w-1/2 lg:text-left text-left space-y-4">
                     <h3 className="text-3xl font-Clash font-semibold">Pawfect Match Website</h3>
                     <ProjectKeywords bgColor="bg-custom-baby-green" keywords={["React", "NodeJS", "AWS"]} />
                     <p>Pawfect Match simulates features which lets users browse pets by category and sends a personalized confirmation email after selection. The website is built and hosted using Docker and AWS.</p>
@@ -37,8 +38,8 @@ export const ProjectsPage = () => {
                     </a>
                 </div>
 
-                <div className="md:w-1/2 md:text-left space-y-5">
-                    <div className="bg-red-500 px-6 md:px-10 pt-6 md:pt-8 pb-10 md:pb-14 shadow-project-shadow-left md:shadow-project-shadow-right rounded-lg border-3 border-black">
+                <div className="lg:w-1/2 lg:text-left space-y-5">
+                    <div className="bg-red-500 px-6 lg:px-10 pt-6 lg:pt-8 pb-10 lg:pb-14 shadow-project-shadow-left lg:shadow-project-shadow-right rounded-lg border-3 border-black">
                         <video className="w-full h-full border-3 border-black" autoplay controls>
                             <source src="/video/Pawfect Match.mp4"></source>
                             Pawfect Match run-through video    
@@ -47,14 +48,14 @@ export const ProjectsPage = () => {
                 </div>
             </div>
 
-            <div className='container mx-auto px-10 md:px-24 mb-20 md:mb-32 md:flex md:flex-row justify-between items-center gap-10'>
-                <div className="md:max-w-1/2 md:text-left space-y-5">
-                    <div className="bg-purple-400 px-6 md:px-10 pt-6 md:pt-8 pb-10 md:pb-14 shadow-project-shadow-left rounded-lg border-3 border-black">
+            <div className='container mx-auto px-10 lg:px-24 mb-20 lg:mb-32 lg:flex lg:flex-row justify-between items-center gap-10'>
+                <div className="lg:max-w-1/2 lg:text-left space-y-5">
+                    <div className="bg-purple-400 px-6 lg:px-10 pt-6 lg:pt-8 pb-10 lg:pb-14 shadow-project-shadow-left rounded-lg border-3 border-black">
                         <img src="/projects-img/Portfolio.png" alt="Portfolio website"></img>
                     </div>
                 </div>
 
-                <div className="md:w-1/2 md:text-left mt-10 md:mt-0 text-left space-y-4">
+                <div className="lg:w-1/2 lg:text-left mt-10 lg:mt-0 text-left space-y-4">
                     <h3 className="text-3xl font-Clash font-semibold">Personal Portfolio</h3>
                     <ProjectKeywords bgColor="bg-orange-300" keywords={["React", "NodeJS", "Figma"]} />
                     <p>What can I say more? You're looking at it! This website summarizes all of my experiences as well as projects I have done so far.</p>
@@ -64,18 +65,19 @@ export const ProjectsPage = () => {
                 </div>
             </div>
 
-            <div className="container mx-auto px-24 mt-20">
+            {/* Mobile Design Section */}
+            <div className="container mx-auto px-10 lg:px-24 mt-20">
                 <h2 className="text-4xl font-Clash font-semibold text-left mt-14 mb-6">Mobile Design</h2>
             </div>
 
-            <div className='container mx-auto px-24 mb-32 flex justify-between items-center gap-10'>
-                <div className="md:max-w-1/2 md:text-left space-y-5">
-                    <div className="bg-yellow-200 px-10 pt-8 pb-14 shadow-project-shadow-left rounded-lg border-3 border-black">
+            <div className='container mx-auto px-10 lg:px-24 mb-20 lg:mb-32 lg:flex lg:flex-row justify-between items-center gap-10'>
+                <div className="lg:max-w-1/2 lg:text-left space-y-5">
+                    <div className="bg-yellow-200 px-7 lg:px-10 pt-7 lg:pt-8 pb-10 lg:pb-14 shadow-project-shadow-left rounded-lg border-3 border-black">
                         <img src="/projects-img/NavigateDal.png" alt="NavigateDal mobile design"></img>
                     </div>
                 </div>
 
-                <div className="md:w-1/2 md:text-left text-left space-y-4">
+                <div className="lg:w-1/2 lg:text-left mt-10 lg:mt-0 text-left space-y-4">
                     <h3 className="text-3xl font-Clash font-semibold">NavigateDal App</h3>
                     <ProjectKeywords bgColor="bg-custom-baby-blue" keywords={["Figma", "UI Design"]} />
                     <p>NavigateDal is a mobile and wearable high-fidelity prototype for university navigation app, designed to help students navigate within campus. It is developed as part of a 4-month project with a team of 6, involving brainstorming, research, data collection and peer reviews.</p>
@@ -89,8 +91,8 @@ export const ProjectsPage = () => {
                 </div>
             </div>
 
-            <div className='container mx-auto px-24 mb-32 flex justify-between items-center gap-10'>
-                <div className="md:w-1/2 md:text-left text-left space-y-4">
+            <div className='container mx-auto px-10 lg:px-24 mb-20 lg:mb-32 flex flex-col-reverse lg:flex-row justify-between items-center gap-10'>
+                <div className="lg:w-1/2 lg:text-left text-left space-y-4">
                     <h3 className="text-3xl font-Clash font-semibold">SafelyHome App</h3>
                     <ProjectKeywords bgColor="bg-red-300" keywords={["Figma", "UI Design"]} />
                     <p>SafelyHome is a high-fidelity mobile app prototype that offers safe routes for users navigating alone. It is developed over 4 months in collaboration with a peer, focusing on research and design for vulnerable population, specifically women.</p>
@@ -99,14 +101,15 @@ export const ProjectsPage = () => {
                     </a>
                 </div>
 
-                <div className="md:max-w-1/2 md:text-left space-y-5">
-                    <div className="bg-lime-300 px-10 pt-8 pb-14 shadow-project-shadow-right rounded-lg border-3 border-black">
+                <div className="lg:max-w-1/2 lg:text-left space-y-5">
+                    <div className="bg-lime-300 px-7 lg:px-10 pt-7 lg:pt-8 pb-10 lg:pb-14 shadow-project-shadow-left lg:shadow-project-shadow-right rounded-lg border-3 border-black">
                         <img src="/projects-img/SafelyHome.png" alt="SafelyHome mobile design"></img>
                     </div>
                 </div>
             </div>
 
-            <div className="container mx-auto px-24 mt-20 mb-32">
+            {/* Coloring Section */}
+            <div className="container mx-auto px-10 lg:px-24 mt-20 mb-32">
                 <h2 className="text-4xl font-Clash font-semibold text-left mt-14 mb-2">Coloring</h2>
                 <p className="text-left mb-6">Please enjoy some colors I have done as a hobby!</p>
                 <ImageSlider />
