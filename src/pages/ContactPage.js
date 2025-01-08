c0b1c5e3405d3e73088f268a468f76208438438f
Lam Tieu <tieunhatlam1210@gmail.com>
2025-01-08 00:34:58 -0400

    Add responsive style for tablet view in Contact page

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -4,11 +4,11 @@ import Form from "../components/Form";
 export const ContactPage = () => {
     return (
         <>
-            <div className='container mx-auto px-12 md:px-24 mt-20 mb-32 md:flex md:justify-between md:items-center'>
-                <div className="md:w-1/2 text-left space-y-5">
-                    <h1 className="text-6xl md:text-7xl font-Clash">Let's Talk!</h1>
+            <div className='container mx-auto px-12 md:px-24 mt-20 mb-32 lg:flex lg:justify-between lg:items-center'>
+                <div className="lg:w-1/2 text-left space-y-5">
+                    <h1 className="text-6xl lg:text-7xl font-Clash">Let's Talk!</h1>
                     <p>I'd love to hear from you! Whether you have a question, feedback, or just want to say hi, feel free to shoot me a message!</p>
-                    <ul className="py-6 md:pb-0 md:pt-16 space-y-1">
+                    <ul className="py-6 lg:pb-0 lg:pt-16 space-y-1">
                         <li>
                             <ContactLink src={"Gmail"} text={"tieunhatlam1210@gmail.com"} />
                         </li>
