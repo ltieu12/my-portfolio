09c0ce2d518c738af37a14cef06db24e5ba69824
Lam Tieu <tieunhatlam1210@gmail.com>
2025-01-08 14:25:15 -0400

    Fix SVG attributes to resolve console errors

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,9 +10,9 @@ export default function NavBar() {
                     <a href="/" className='text-3xl md:text-4xl font-Clash'>LamTieu</a>
 
                     <button type="button" onClick={() => setIsOpen(!isOpen)} className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-black">
-                        <span class="sr-only">Open main menu</span>
-                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                        <span className="sr-only">Open main menu</span>
+                        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
                         </svg>
                     </button>
 
